82d72ad864be17a25710554429df6bdd0045e103
alexandrobs <jornalsinal@gmail.com>
2023-11-25 23:29:19 -0300

    Estudos de coroutines
    1 importando a dependencia no gradle
    2 básico de uma coroutina, runBlocking, suspend fun, coroutineScope, launch e delay

--- app/src/main/java/com/absdev/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/absdev/android/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.absdev.android.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
+
+    }
+
+    fun main () = runBlocking {
+        launch { doWorld() }
+        println("Hello")
+    }
+
+    suspend fun doWorld() {
+        delay(1000)
+        println("World!")
     }
 }
