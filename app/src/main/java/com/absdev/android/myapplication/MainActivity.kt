a9ca5711ab8e02bdee648a2b901436f56dcd25b2
alexandrobs <jornalsinal@gmail.com>
2023-11-27 22:45:18 -0300

    Estudos Android Básico
    1 Button, setOnClickListener, Toast e RelativeLayout

--- app/src/main/java/com/absdev/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/absdev/android/myapplication/MainActivity.kt
@@ -1,25 +1,23 @@
 package com.absdev.android.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.*
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main()
+        // Referenciar o botão no código
+        val meuBotao: Button = findViewById(R.id.meuBotao)
 
-    }
-
-    fun main () {
-        val job = GlobalScope.launch {
-            delay(1000)
-            println("Corotina executada!")
+        // Configurar um ouvinte de clique para o botão
+        meuBotao.setOnClickListener {
+            // Ação a ser realizada quando o botão for clicado
+            Toast.makeText(this, "Botão clicado!", Toast.LENGTH_SHORT).show()
         }
-
-        //cancelar corotina quando necessario
-        job.cancel()
     }
 }
